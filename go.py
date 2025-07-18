6a578af1be77a06739ab73051aeac7cf8c392d06
Tharindu Lakshan <fiverrmoney1324@gmail.com>
2025-07-18 17:27:17 +0530

    Update go.py

--- go.py
+++ go.py
@@ -1,2 +1,9 @@
 print("This is done bro again")
 
+def create_hello_file(filename):
+    with open(filename, 'w') as file:
+        file.write("Hello, world!")
+
+# Demo usage
+create_hello_file("hello.txt")
+print("File 'hello.txt' created with message.")
