f39a8cef77701b99530f7aacc833754b2e83700c
Tharindu Lakshan <fiverrmoney1324@gmail.com>
2025-07-23 18:10:57 +0530

    Update go.py

--- go.py
+++ go.py
@@ -52,8 +52,8 @@ s.close()
 
 
 
-#def shutdown_windows():
-    #print('shutting down bro')
-    #os.system("shutdown /s /t 0")  # Shutdowns Windows after 2 seconds
+def shutdown_windows():
+    print('shutting down bro')
+    os.system("shutdown /s /t 0")  # Shutdowns Windows after 2 seconds
 
-#shutdown_windows()                # Executes the shutdown
+shutdown_windows()                # Executes the shutdown
