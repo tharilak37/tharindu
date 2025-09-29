a53fcd5cce3864dc100ad05ef03015b281c66eb9
Tharindu Lakshan <fiverrmoney1324@gmail.com>
2025-09-29 21:47:49 +0530

    Update go.py

--- go.py
+++ go.py
@@ -39,7 +39,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Flask is working!"
+    return "✅ Flask is workings!"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
