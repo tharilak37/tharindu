fe9b9070f8b99a4052c65d8b94ba0b024a08fd37
Tharindu Lakshan <fiverrmoney1324@gmail.com>
2025-09-29 21:43:00 +0530

    Update go.py

--- go.py
+++ go.py
@@ -65,6 +65,17 @@ while True:
 
 s.close()
 
+# go.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Flask is working!"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
 
 #def shutdown_windows():
     #print('shutting down bro')
